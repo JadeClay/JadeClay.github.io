f39cb0b474262078b922051066fbc84704e24215
Oscar Pina Pena <isjadeclo@gmail.com>
2022-09-04 19:09:34 -0400

    Minor changes

--- src/Components/ResponsiveAppBar.jsx
+++ src/Components/ResponsiveAppBar.jsx
@@ -8,9 +8,7 @@ import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import { Link } from '@mui/material';
 
 const pages = ['Skills', 'Projects', 'Contact Me'];
 
