ca9584dd0df4560ba79131ac4a7092c5268c2f69
Oscar Pina Pena <isjadeclo@gmail.com>
2022-09-30 22:06:00 -0400

    Cosmetic changes to footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,23 +1,26 @@
 import { FavoriteOutlined } from '@mui/icons-material'
 import CopyrightIcon from '@mui/icons-material/Copyright';
-import { Container, Typography, Stack } from '@mui/material'
+import { Container, Typography, Stack, Box } from '@mui/material'
 import React, { Component } from 'react'
 
 export default class Footer extends Component {
   render() {
     return (
-      <Container sx={{ padding: 2 }}>
-        <Stack direction="row" alignItems="center" gap={0.5} justifyContent="center" display={{ xs:'inline-flex', md:'none' }}>
-          <CopyrightIcon fontSize='small' />
-          <Typography variant="subtitle1">2022. All rights reserved. </Typography>
-        </Stack>
-        <Stack direction="row" alignItems="center" gap={0.5} justifyContent="center" display={{ xs:'none', md:'inline-flex' }}>
-          <CopyrightIcon fontSize='small' />
-          <Typography variant="subtitle1">2022. Done with </Typography>
-          <FavoriteOutlined fontSize='small' htmlColor='#93000a' />
-          <Typography variant="subtitle1">by Oscar Piña, using Material UI and React.js</Typography>
-        </Stack>
-      </Container>
+      <Box sx={{backgroundColor: '#0f0f0f'}}>
+        <Container sx={{ padding: 2 }}>
+          <Stack direction="row" alignItems="center" gap={0.5} justifyContent="center" display={{ xs:'inline-flex', md:'none' }}>
+            <CopyrightIcon fontSize='small' />
+            <Typography variant="subtitle1">2022. All rights reserved. </Typography>
+          </Stack>
+          <Stack direction="row" alignItems="center" gap={0.5} justifyContent="center" display={{ xs:'none', md:'inline-flex' }}>
+            <CopyrightIcon fontSize='small' />
+            <Typography variant="subtitle1">2022. Done with </Typography>
+            <FavoriteOutlined fontSize='small' htmlColor='#93000a' />
+            <Typography variant="subtitle1">by Oscar Piña, using Material UI and React.js</Typography>
+          </Stack>
+        </Container>
+      </Box>
+
     )
   }
 }
